b049ff95a702d740c7b02ed839dfd746c3dc6c4d
vicmui <muiheiman@gmail.com>
2026-02-09 16:40:13 +0800

    Fix CSS style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // 防止你用左 src 目錄
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
     extend: {
